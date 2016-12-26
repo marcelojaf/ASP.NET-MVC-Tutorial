b17043d6f8fee928430c55005d19e3322862dbcc
Marcelo Amador <marcelojaf@gmail.com>
2016-12-25 23:09:05 -0500

    Implemented the SaveChanges method to consider DataCadastro property

--- src/EP.CursoMvc.Infra.Data/Context/CursoMvcContext.cs
+++ src/EP.CursoMvc.Infra.Data/Context/CursoMvcContext.cs
@@ -41,6 +41,23 @@ namespace EP.CursoMvc.Infra.Data.Context
 
             base.OnModelCreating(modelBuilder);
         }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+            return base.SaveChanges();
+        }
     }
 }
 
