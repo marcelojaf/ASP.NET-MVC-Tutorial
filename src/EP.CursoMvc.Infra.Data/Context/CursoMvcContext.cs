143d47ffd90992100430745132cb2faa28254ba5
Marcelo Amador <marcelojaf@gmail.com>
2016-12-25 19:31:12 -0500

    Enabled Migrations on Data project and inserted some model config like Key properties or set maximum varchar size

--- src/EP.CursoMvc.Infra.Data/Context/CursoMvcContext.cs
+++ src/EP.CursoMvc.Infra.Data/Context/CursoMvcContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,5 +18,24 @@ namespace EP.CursoMvc.Infra.Data.Context
 
         public DbSet<Cliente> Clientes { get; set; }
         public DbSet<Endereco> Enderecos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Properties()
+                .Where(p => p.Name == p.ReflectedType.Name + "Id")
+                .Configure(p => p.IsKey());
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar"));
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasMaxLength(100));
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
