ab2cd83c4a9d210cd592e3104da32e040580c541
Marcelo Amador <marcelojaf@gmail.com>
2016-12-25 19:22:15 -0500

    Created the Data project, the DbContext file and installed the Entity Framework

---
+++ src/EP.CursoMvc.Infra.Data/Context/CursoMvcContext.cs
@@ -0,0 +1,21 @@
+﻿using EP.CursoMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EP.CursoMvc.Infra.Data.Context
+{
+    public class CursoMvcContext : DbContext
+    {
+        public CursoMvcContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+    }
+}
